49e24a4b8453102cd1fcc01346852fbd5a2c818e
amxrac <108365444+amxrac@users.noreply.github.com>
2025-07-07 22:19:42 +0100

    feat: add init_db fn

--- src/main.rs
+++ src/main.rs
@@ -2,12 +2,13 @@ use std::error::Error;
 use std::fs::File;
 use std::io::BufReader;
 use rss::Channel;
+use sqlx::sqlite::SqlitePool;
 
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn Error>> {
+    init_db();
     parse_xml("https://news.ycombinator.com/rss").await?;
-    // println!("{:?}", response);
     Ok(())
 }
 
@@ -20,9 +21,24 @@ async fn parse_xml(xml: &str) -> Result<Channel, Box<dyn Error>> {
     let content = fetch_rss(xml).await?;
     let channel = Channel::read_from(content.as_bytes())?;
     for item in &channel.items {    
-        println!("{:?}", item.title.as_deref().unwrap_or(""));
-        println!("{:?}", item.link.as_deref().unwrap_or(""));
+        println!("{}", item.title.as_deref().unwrap_or(""));
+        println!("{}", item.link.as_deref().unwrap_or(""));
+        println!("{}", item.pub_date.as_deref().unwrap_or(""));
     }
     Ok(channel)
 }
 
+async fn init_db() -> Result<(), Box<dyn Error>> {
+    let pool = SqlitePool::connect("sqlite:feeds.db?mode=rwc").await?;
+    sqlx::query(
+        r#"
+        CREATE TABLE IF NOT EXISTS feeds (
+            id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
+            title TEXT NOT NULL,
+            url TEXT UNIQUE NOT NULL,
+            published_at DATETIME NOT NULL
+        )
+        "#
+    ).execute(&pool).await?;
+    Ok(())
+}

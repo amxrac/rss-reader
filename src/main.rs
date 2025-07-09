0fdec4e4cfad3e9f6a8552c5e0f1b3b3502e33c5
amxrac <108365444+amxrac@users.noreply.github.com>
2025-07-09 13:09:17 +0100

    feat: add add_feed fn

--- src/main.rs
+++ src/main.rs
@@ -2,16 +2,20 @@ use std::error::Error;
 use std::fs::File;
 use std::io::BufReader;
 use clap::builder::Str;
+use clap::Parser;
 use rss::Channel;
 use sqlx::sqlite::SqlitePool;
+use sqlx::Sqlite;
+use std::io;
+use chrono::prelude::*;
 
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn Error>> {
     init_db().await?;
-    // add_feed
-    // validate_rss
-    parse_xml("https://news.ycombinator.com/rss").await?;
+    // parse_xml("https://news.ycombinator.com/rss").await?;
+    let pool = SqlitePool::connect("sqlite:feeds.db").await?;
+    add_feed(&pool).await?;
     Ok(())
 }
 
@@ -37,9 +41,46 @@ async fn init_db() -> Result<(), Box<dyn Error>> {
     Ok(())
 }
 
-async fn validate_rss(url: &str) -> Result<String, Box<dyn Error>> {
+async fn validate_rss(url: &str) -> Result<String, String> {
     let response = reqwest::get(url).await.map_err(|_|"failed to fetch url")?; 
     let content = response.text().await.map_err(|_| "failed to read content")?;
     let channel = Channel::read_from(content.as_bytes()).map_err(|_| "invalid rss format")?;
     Ok(channel.title)
+}
+
+async fn add_feed(pool: &SqlitePool) -> Result<(), Box<dyn Error>> {
+    println!("enter rss feed url: ");
+    let mut url: String = String::new();
+
+
+    loop {
+        url.clear();
+
+        io::stdin()
+            .read_line(&mut url)
+            .expect("failed to read line");
+
+        match validate_rss(&url).await {
+            Ok(title) => {
+                sqlx::query(
+                    r#"
+                    INSERT INTO feeds (title, url, published_at) VALUES (?, ?, ?)
+                    "#
+                )
+                .bind(title)
+                .bind(&url.trim())
+                .bind(chrono::Utc::now().to_rfc3339())
+                .execute(pool)
+                .await?;
+                println!("rss feed added successfully");
+                break url.trim().to_string();
+            }
+            Err(err) => {
+                println!("Error adding feed: {}", err);
+                continue;
+            }
+        }
+
+    };
+    Ok(())
 }
